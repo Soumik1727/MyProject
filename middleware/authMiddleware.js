01dfcbfb3322ff1e8588fcea124d979d444f7227
Soumik Mondal <soumikmondal1727@gmail.com>
2024-11-06 01:22:22 +0530

    favicon

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,7 +11,7 @@ export const authenticateUser = (req, res, next) => {
 
   try {
     const { userId, role } = verifyJWT(token);
-    const testUser = userId === '64b2c07ccac2efc972ab0eca';
+    const testUser = userId === '672a7688eb00c4a1cf17944e';
     req.user = { userId, role, testUser };
     next();
   } catch (error) {
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -17,17 +17,29 @@ import { checkForTestUser } from '../middleware/authMiddleware.js';
 // router.get('/',getAllJobs)
 // router.post('/',createJob)
 
-router
+// router
+//   .route('/')
+//   .get(getAllJobs)
+//   .post(checkForTestUser, validateJobInput, createJob);
+
+// router.route('/stats').get(showStats);
+
+// router
+//   .route('/:id')
+//   .get(validateIdParam, getJob)
+//   .patch(checkForTestUser, validateJobInput, validateIdParam, updateJob)
+//   .delete(checkForTestUser, validateIdParam, deleteJob);
+
+  router
   .route('/')
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
-
-router.route('/stats').get(showStats);
 
 router
   .route('/:id')
   .get(validateIdParam, getJob)
   .patch(checkForTestUser, validateJobInput, validateIdParam, updateJob)
   .delete(checkForTestUser, validateIdParam, deleteJob);
+
 
 export default router;
